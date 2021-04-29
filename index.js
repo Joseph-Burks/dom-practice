937b7c5f1a77196e27e2f0005d9419cd34ba2522
Joseph-Burks <joe_burks_3@yahoo.com>
2021-04-29 16:13:12 -0500

    more pets and deliverables

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const pets = [
 	{ owner: 'Ivan', type: 'Dog', name: 'CJ' },
 	{ owner: 'Stefan', type: 'Fish', name: 'Goldie' },
 	{ owner: 'Keith', type: 'Dog', name: 'Sydney' },
-	{ owner: '', type: '', name: '' },
-	{ owner: '', type: '', name: '' },
-	{ owner: '', type: '', name: '' },
-	{ owner: '', type: '', name: '' },
-	{ owner: '', type: '', name: '' },
+	{ owner: 'Jay', type: 'Dog', name: 'Momo' },
+	{ owner: 'Chris', type: 'Cat', name: 'Sir Charles' },
+	{ owner: 'Tony', type: 'Dog', name: '' },
+	{ owner: 'Vic', type: 'Dog', name: 'Pixl' },
+	{ owner: 'Katelyn', type: 'Cat', name: 'BatMann' },
 ];
+
+// console.log the pets array
+
+// console.log each of the pet names
+
+// add a title to the page with an h1
+
+// insert a list item for each owner's name in the ul with id='owners'
+
+// insert a list item for each pet's name in the ul with id='pet-names'
+
+// insert a list of pet types with the number of each in the ul with id='pet-types'
+
+// insert a list of owners and their pets in the div with id='owner-pets'
+// {Owner} has a {pet type} named {pet name}.
+
+// Practice styling by updating the DOM with JS...
