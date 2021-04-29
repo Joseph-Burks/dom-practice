bab65e30b5e57e52d3b7c4c13304a8b9c5a1b683
Joseph-Burks <joe_burks_3@yahoo.com>
2021-04-29 15:06:27 -0500

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+const pets = [
+	{ onwer: 'Joe', type: 'Dog', name: 'Taco' },
+	{ owner: 'Megan', type: 'Cat', name: 'Biscuit' },
+	{ owner: 'Ivan', type: 'Dog', name: 'CJ' },
+	{ owner: 'Stefan', type: 'Fish', name: 'Goldie' },
+	{ owner: 'Keith', type: 'Dog', name: 'Sydney' },
+	{ owner: '', type: '', name: '' },
+	{ owner: '', type: '', name: '' },
+	{ owner: '', type: '', name: '' },
+	{ owner: '', type: '', name: '' },
+	{ owner: '', type: '', name: '' },
+];
